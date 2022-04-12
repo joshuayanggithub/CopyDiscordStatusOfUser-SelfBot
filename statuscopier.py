889752bfab0e5fa7b739187bfd0ccf8a9d717ef4
Certified Baller <85262856+joshuayanggithub@users.noreply.github.com>
2022-04-11 20:36:51 -0700

    Update statuscopier.py

--- statuscopier.py
+++ statuscopier.py
@@ -1,12 +1,15 @@
 import discord, json ,time, requests
 from discord.ext import commands,tasks
 
-f = open( "info.txt" ,"r")
+f = open( "UserToken&IDs.txt" ,"r")
 statuschange_url = "https://discord.com/api/v9/users/@me/settings"
 token = str(f.readline().strip())
 user_id = int (f.readline().strip()) #test/sample id = 962488616058245140
 sharedguild_id = int (f.readline().strip()) #test/sample id = 962491841293467650
-oldstatus = ""
+f.close()
+
+f2 = open("oldstatus.txt","r")
+oldstatus = f2.readline().strip()
 
 intents = discord.Intents.all()
 intents.members = True
@@ -18,7 +21,7 @@ def formattedPrint(str):
 	print(str)
 	print("------------------------------------------------------------------------------------")
 def getFormattedTime():
-    return time.strftime('%H:%M%p %Z on %b %d, %Y')
+    return time.strftime('%I:%M %p %Z on %b %d, %Y')
 def changeStatus(message):
 	payload = {
 		"status": "online",
@@ -32,7 +35,10 @@ def changeStatus(message):
 	}
 	r = requests.patch(statuschange_url, data=json.dumps(payload), headers=headers)
 	if (r.status_code == 200): #success
-		formattedPrint("Succesfully changed status to\n" + message + "\nAt " + getFormattedTime())
+		formattedPrint("Succesfully changed status to\n\" " + message + " \"\nAt " + getFormattedTime())
+		f3 = open("oldstatus.txt","w")
+		f3.write(message)
+		f3.close()
 	else: #400
 		formattedPrint("Error in changing status..." + getFormattedTime())
 def storeOldStatus(status):
@@ -55,8 +61,12 @@ async def checkStatus():
 			if (status != oldstatus):
 				oldstatus = status
 				changeStatus(status)
+			else:
+				formattedPrint("Your status matches person's status already | " + getFormattedTime())
+		else:
+			formattedPrint("Person currently has no status | " + getFormattedTime())
 	else:
 		await bot.change_presence(status=discord.Status.offline)
-		formattedPrint("Target User Not Online at " + getFormattedTime())
+		formattedPrint("Target User Not Online | " + getFormattedTime())
 
 bot.run(token, bot=False)
